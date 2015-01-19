3e99f8672654fee0196fe47cac34aee981c19a8d
jmsaavedra <mailbox.jms@gmail.com>
2015-01-19 12:44:06 -0500

    implemented callback on OSC msgs

--- OF_APP/src/settings.h
+++ OF_APP/src/settings.h
@@ -13,7 +13,7 @@
 #define USE_DEBUG_VIDEO //COMMENT OUT TO USE LIVE CAM
 #define CAM_DEVICE_ID 0
 
-#define LEFT_SCREEN_IP  "192.168.1.10" //MASTER (left screen)
+#define LEFT_SCREEN_IP  "192.168.1.25" //MASTER (left screen)
 #define RIGHT_SCREEN_IP "192.168.1.26" //SLAVE (right screen)
 
 #define NUM_CATEGORIES 7
